94c94a1717f65ac294b477cf15a202641d982035
Rohit152002 <laishramrohit15@gmail.com>
2024-03-29 09:31:58 +0530

    cors 2

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const studentRoute = require("./router/student.routes");
 require("dotenv").config();
 const mongodb = require("./config/db");
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: "*",
