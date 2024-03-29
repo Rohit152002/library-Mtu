618b287ef931ffc8599d6fcfd973ffc04b02269a
Rohit152002 <laishramrohit15@gmail.com>
2024-03-29 09:29:25 +0530

    cors 2

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ require("dotenv").config();
 const mongodb = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
