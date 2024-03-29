2a26fb5e940f5d7e696ed5d88bea81fa203a22f2
Rohit152002 <laishramrohit15@gmail.com>
2024-03-29 09:45:58 +0530

    cors 2

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const studentRoute = require("./router/student.routes");
 require("dotenv").config();
 const mongodb = require("./config/db");
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 const PORT = process.env.PORT || 3000;
 // const corsOptions = {
 //   origin: "*",
 //   credentials: true, //access-control-allow-credentials:true
 //   optionSuccessStatus: 200,
 // };
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
