269fdf351cc2b9ce6ad2b36782eaa386984dcc1f
Rohit152002 <laishramrohit15@gmail.com>
2024-04-08 15:01:18 +0530

    samesite

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
-
+import dotenv from "dotenv";
+dotenv.config();
 const generateToken = (res, studentId) => {
   const token = jwt.sign({ studentId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -7,7 +8,7 @@ const generateToken = (res, studentId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
