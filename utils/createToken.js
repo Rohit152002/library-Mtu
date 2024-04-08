f68b47a48a1d7cdfae97d5d340bffa8b893646a7
Rohit152002 <laishramrohit15@gmail.com>
2024-04-08 14:57:58 +0530

    samesite

--- utils/createToken.js
+++ utils/createToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, studentId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
