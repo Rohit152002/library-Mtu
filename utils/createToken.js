b4637c913d07bb24ddcd15a3bc294714a9589cc5
Rohit152002 <laishramrohit15@gmail.com>
2024-04-10 14:55:37 +0530

    cors

--- utils/createToken.js
+++ utils/createToken.js
@@ -9,7 +9,6 @@ const generateToken = (res, studentId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     // secure: true,
-    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
