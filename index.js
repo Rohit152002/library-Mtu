2f3a4b9ecc375f6e1595918c2410c69ae832f239
Rohit152002 <laishramrohit15@gmail.com>
2024-05-20 14:57:28 +0530

    cors-new

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://librarymanagmentweb-nc9bnpvql-konthoujam-lanthoibas-projects.vercel.app/",
+      "https://librarymanagmentweb.vercel.app/",
       "http://localhost:5173",
     ],
     credentials: true,
